5b9ba07fd3e0ae8c272caaa8862aea9403b81ad9
ahirschberg <googlestinks@ymail.com>
2014-09-29 14:14:55 -0400

    Added comments to threading_queue.rb

--- ruby/threading_queue.rb
+++ ruby/threading_queue.rb
@@ -1,23 +1,26 @@
+# Test class for managing downloading and parsing in separate threads
+# Commented to be readable for someone not familiar with ruby
 class Parser
-  END_PARSE_SYM = :END_PARSE
+  @@END_PARSE_SYM = :END_PARSE # class constant which signals that there are no more items to be parsed
+
   def initialize()
-    @queue = Queue.new
+    @queue = Queue.new # queue is a thread safe list-like element 
   end
 
   def add(*files)
-    files.each {|file| @queue << file } #better way to do this?
+    files.each {|file| @queue << file } # add each file to the queue
   end
 
   def finish
     puts "Sending finish message to queue"
-    @queue << END_PARSE_SYM
+    @queue << @@END_PARSE_SYM
   end
 
   def parse # the code that calls this method is responsible for threading
     while true do
       puts "Waiting for queue" if @queue.empty?
-      file = @queue.pop
-      break if file == END_PARSE_SYM
+      file = @queue.pop # this method is blocking if @queue is empty
+      break if file == @@END_PARSE_SYM # if the end parse message is encountered, break the loop
       puts "Beginning parse of #{file}..."
       sleep rand() * 2
       puts "Parsed #{file}."
@@ -38,8 +41,8 @@ class Downloader
 end
 
 parser = Parser.new 
-thread = Thread.new { parser.parse }
+thread = Thread.new { parser.parse } # call parser.parse in a new thread
 downloader = Downloader.new parser, %w[file1 file2 file3]
-parser.finish
+parser.finish # let parser know that there are no more files to wait for
 thread.join
 puts "Parsing completed."
