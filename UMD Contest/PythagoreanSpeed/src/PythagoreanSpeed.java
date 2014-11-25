4d41c0ed0c946dbca034727efed7789bc9fca412
ahirschberg <googlestinks@ymail.com>
2014-11-25 14:17:44 -0500

    Updated pythagorean speed to use nanoseconds, improved output, and added support for testing multiple values of n

--- UMD Contest/PythagoreanSpeed/src/PythagoreanSpeed.java
+++ UMD Contest/PythagoreanSpeed/src/PythagoreanSpeed.java
@@ -5,46 +5,51 @@ import java.util.Arrays;
 import java.util.Random;
 public class PythagoreanSpeed {
     public static void main(String[] args) {
-        int numPasses = 30;
+        int passes = 100;
+        int[] amounts = {50, 100, 500, 1000};
         Pythagorean[] strategies = {new Pythagorean1(), new Pythagorean2(), new Pythagorean3(), new Pythagorean4()};
-        long[][] timings = new long[strategies.length][numPasses + 1];
+        long[][][] timings = new long[amounts.length][strategies.length][passes + 1];
 
-        System.out.print("Iteration...");
-        for (int iter = 1; iter <= numPasses; iter++) {
-            System.out.print(iter + " ");
-            int[] numbers = new int[1000];
-            int lastCount = -1;
-            Random rand = new Random();
-            for (int i = 0; i < numbers.length; i++) {
-                numbers[i] = rand.nextInt(1000) + 1;
-            }
-
-            Arrays.sort(numbers);
-            for (int strat_index = 0; strat_index < strategies.length; strat_index++) {
-                Pythagorean strategy = strategies[strat_index];
-                long startTime = System.currentTimeMillis();
-                int count = strategy.solve(numbers);
-                timings[strat_index][iter] = (System.currentTimeMillis() - startTime);
-
-                if (lastCount != -1 && lastCount != count) {
-                    System.err.println("count " + count + " did not equal lastCount "
-                            + lastCount + " for strategy: " + strategy.getName());
-                    break;
+        for (int amounts_index = 0; amounts_index < amounts.length; amounts_index++) {
+            int nTerms = amounts[amounts_index];
+            System.out.print("n=" + nTerms + "; Iteration:");
+            for (int iter = 1; iter <= passes; iter++) {
+                System.out.print(" " + iter);
+                int[] numbers = new int[nTerms];
+                int lastCount = -1;
+                Random rand = new Random();
+                for (int i = 0; i < numbers.length; i++) {
+                    numbers[i] = rand.nextInt(nTerms) + 1;
                 }
 
-                lastCount = count;
-            }
-        }
+                Arrays.sort(numbers);
+                for (int strat_index = 0; strat_index < strategies.length; strat_index++) {
+                    Pythagorean strategy = strategies[strat_index];
+                    long startTime = System.nanoTime();
+                    int count = strategy.solve(numbers);
+                    timings[amounts_index][strat_index][iter] = (System.nanoTime() - startTime);
+                    System.out.print(".");
+                    if (lastCount != -1 && lastCount != count) {
+                        System.err.println("count " + count + " did not equal lastCount "
+                                + lastCount + " for strategy: " + strategy.getName());
+                        break;
+                    }
 
-        System.out.println("\nTimings:");
-        for (int strategy_index = 0; strategy_index < timings.length; strategy_index++)
-        {
-            long average = 0L;
-            for (int timing_index = 0; timing_index < timings[strategy_index].length; timing_index++)
-            {
-                average += timings[strategy_index][timing_index];
+                    lastCount = count;
+                }
             }
-            System.out.println("Strategy " + strategies[strategy_index].getName() + ": " + average / timings[strategy_index].length + " ms.");
+            System.out.println();
+        }
+        System.out.println("\nTimings:");
+        for (int amounts_index = 0; amounts_index < timings.length; amounts_index++) {
+            System.out.println("=======(n = " + amounts[amounts_index] + ")=======");
+            for (int strategy_index = 0; strategy_index < timings[amounts_index].length; strategy_index++) {
+                long average = 0L;
+                for (int timing_index = 0; timing_index < timings[amounts_index][strategy_index].length; timing_index++) {
+                    average += timings[amounts_index][strategy_index][timing_index];
+                }
+                System.out.println("Strategy " + strategies[strategy_index].getName()+ ": " + average / (double)timings[amounts_index][strategy_index].length / 1000000D + " ms.");
+            }
         }
     }
 
